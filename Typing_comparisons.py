2132b156321305c82ab52335b27d2fb7adee911e
GStenhouse <g.e.stenhouse@liverpool.ac.uk>
2023-02-21 12:29:36 +0000

    Tweaks to the granularity comparisons

--- Typing_comparisons.py
+++ Typing_comparisons.py
@@ -37,57 +37,28 @@ def pairingtab(df_in, list):
 
 
 # Identify matching groups, with and without isolate numbers
-def compare(piv_dict, d):
+def compare(piv_dict):
     piv_comp_dict = {}            # create empty dicts to collect all  output data - pivot : row and matching columns
     piv_pair_dict = {}                # pivot : pair and number of matches
     out =[]
     for key, piv in piv_dict.items():
         row_names = piv.index.to_series() # create list of row names
-        col_names = piv.columns.to_list()   # create list of column names
         pair_out = []              # create empty list to take the pairs and number objects
         match_dict = {}     # create empty dictionaries for collecting the data - list of matching columns by row
            
-        if d in ["rows", "Rows", "by row", "By rows"] :
-            for i in range(0,len(row_names)):    
-                matches = []                       # create empty list for names of matching columns
-                index = -1                          # set index value for extracting the matching column name
-                name = row_names.iloc[i]            # collect row name
-                data = piv.iloc[i]                      # extract row
-                for cell in data.values.tolist():
-                    index += 1              # add 1 to column number
-        
-                    if cell  > 0:
-                        o_name = col_names[index]       # extract name of matching column
-                        matches.append(o_name)        # add matching column name to list
-                    else:
-                        break
-
+        for i in range(0,len(row_names)):    
+            matches = []                       # create empty list for names of matching columns
+            index = -1                          # set index value for extracting the matching column name
+            name = row_names.iloc[i]            # collect row name
+            data = piv.iloc[i]                      # extract row
+            for cell in data:
+                index += 1              # add 1 to row number
+                if cell  > 0:
+                    o_name = data.index[index]       # extract name of matching column
+                    matches.append(o_name)        # add matching column name to list
                     pair = str(name) + " - " + str(o_name) + ": " + str(cell)   # collect row, column and no. of matching isolates
-                    pair_out.append(pair)           # collect in to a list            
-                match_dict[name] = matches      # add row name and all matching column names to dict
-
-        elif d in ["columns", "Columns", "by column", "By column"] :
-            for i in range(0,len(col_names)):
-                matches = []                       # create empty list for names of matching rows
-                index = -1                          # set index value for extracting the matching row name
-                name = col_names[i]                 # collect column name
-                data = piv[name]                        # extract column
-                for cell in data.values.tolist():
-                    index += 1              # add 1 to row number
-
-                    if cell  > 0 :
-                        o_name = row_names.iloc[index]       # extract name of matching row
-                        matches.append(o_name)        # add matching row name to list
-                    else:
-                        break
-
-                    pair = str(name) + " - " + str(o_name) + ": " + str(cell)
-                    pair_out.append(pair)        
-                match_dict[name] = matches
-
-        else:
-            print("Error: No direction specified")
-            return
+                    pair_out.append(pair)           # collect in to a list         
+            match_dict[name] = matches      # add row name and all matching column names to dict
         
         piv_comp_dict[key] = match_dict     # create dict of matches dict - by pivot name
         piv_pair_dict[key] = pair_out       # create dict of match lists - by pivot name
@@ -99,12 +70,13 @@ def compare(piv_dict, d):
 # generate pairwise comparison stats of granular similarity
 def comp_stats(names, comparisons):
     out_dict = {}
-    out_dict["Pair"] = "Total matches, Exact matches", "Excess matches", "Exact proportion", "Excess porportion", "Skew", "skew proportion"        # create empty dictionary to collect results
+    out_dict = {}        # create empty dictionary to collect results
 
 
     for duo in names:
         pair = duo[0] + " - " + duo[1]
         matches = comparisons[0][pair]     # extract matches by pair
+        output = []
     
         tot_count = 0   # set counters
         exact_count = 0
@@ -126,7 +98,13 @@ def comp_stats(names, comparisons):
         perc_eed = exceed_count/tot_count
         perc_skew = skew/tot_count
 
-        output = repr(tot_count) + ", " + repr(exact_count) + ", " + repr(exceed_count) + ", " + repr(perc_act) + ", " + repr(perc_eed) + ", " + repr(skew) + ", " + repr(perc_skew)
+        output.append(tot_count) 
+        output.append(exact_count) 
+        output.append(exceed_count) 
+        output.append(perc_act) 
+        output.append(perc_eed) 
+        output.append(skew) 
+        output.append(perc_skew)
         out_dict[pair] = output
     return(out_dict)
 
@@ -150,19 +128,19 @@ pair_list = itertools.permutations(columns, r=2)
 # use pairwise list to run pair wise pivot table creation
 pivot_dict = pairingtab(df_in, pair_list)
 
-# use output to identify matching groups
-row_gran_comp_list = compare(pivot_dict, d="rows")
-
-col_gran_comp_list = compare(pivot_dict, d="columns")
+# use output to identify matching groups - don't need to do separate by columns comp because pairingtab create list with pair both ways round
+gran_comp_list = compare(pivot_dict)
 
 # generate comparison stats
 # find the number of times, per pivot pair, that there are exact matches, as a proportion of the total number of matches
 pair_list = itertools.permutations(columns, r=2)
-row_comp_stats = comp_stats(pair_list, row_gran_comp_list)
+comp_stats_out = comp_stats(pair_list, gran_comp_list)
 
-pair_list = itertools.permutations(columns, r=2)
-column_comp_stats = comp_stats(pair_list, col_gran_comp_list)
+# compile comparisons in to dataframe
+comp_stats_df = pd.DataFrame.from_dict(comp_stats_out, orient = "index", columns = ("Total matches", "Exact matches", "Excess matches", "Exact proportion", "Excess porportion", "Skew", "skew proportion"))
 
+# and then send to csv file
+comp_stats_df.to_csv("test_gran_comp.csv", sep=",")
 
 
     
